8c8ebc0d04f75e673148ac3ffdbe0fd3a22bb919
amaiamarana <amaiamarana@gmail.com>
2015-03-10 00:46:28 +0100

    fin nivel 3

--- buscarcaracteres.sh
+++ buscarcaracteres.sh
@@ -9,5 +9,5 @@ echo "Nº líneas que contienen la palabra 'se': " $b;
 c=$(egrep '[a-z]+se[a-z]+ | [Ss]e[a-z]+ | [a-z]+[Ss]e' juegodeprueba.txt | wc -l);
 echo "Nº líneas que contienen 'se' dentro de una palabra: " $c;
 
-d=$(egrep -E '\<[Ss]e\>.*[a-z]+se[a-z]+ | [Ss]e[a-z]+|[a-z]+[Ss]e|[a-z]+se[a-z]+ | [Ss]e[a-z]+ | [a-z]+[Ss]e.*\<[Ss]e\>' juegodeprueba.txt);
-echo "Nº líneas que en la misma línean tiene 'se' como palabra o cadena: "
+d=$(egrep -E '(\<[Ss]e\>.+((se[a-z]+)|([a-z]+se[a-z]+)|([a-z]+se)))|(((se[a-z]+)|([a-z]+se[a-z]+)|([a-z]+se)).+\<[Ss]e\>)' juegodeprueba.txt | wc -l);
+echo "Nº líneas que en la misma línean tiene 'se' como palabra o cadena: " $d;
--- registros.sh
+++ registros.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-a=$(egrep '^unix' /etc/passwd );
-echo $?;
-if ($? != 0 )
-then
-	echo "Usuarios con la palabra Unix al principio: 0";
-fi
-
+a=$(egrep '.*:.*:.*:.*:.*Unix' /etc/passwd | wc -l);
+echo '====================================';
 echo "Usuarios con la palabra Unix al principio: " $a;
 
-b=$(egrep [a-z]:.:...:101 /etc/pass );
+b=$(egrep '.*:.*:.*:101' /etc/passwd | wc -l );
+echo '====================================';
 echo "Usuarios del grupo 101: " $b;
 
-c=$(egrep ':[0-9][0-9][0-9]:101' /etc/passwd);
+c=$(egrep ':[0-9]*:(101|100|105)' /etc/passwd | wc -l );
+echo '====================================';
 echo "Usuarios del grupo 100,101,105: " $c;
+
+d=$(egrep '.*:.*:.*:.*:.*:/home/un.*' /etc/passwd | wc -l );
+echo '====================================';
+echo "Usuarios con directorio propio /home/un*: " $d;
+
+echo '====================================';
+echo "Listar usuarios de 1 dígito: ";
+egrep '.*:.*:[0-9]:.*:.*:.*:.*' /etc/passwd;
+
+echo '====================================';
+echo "Listar usuarios de 1 o 2 dígitos: ";
+egrep '.*:.*:([0-9]|[0-9][0-9]):.*:.*:.*:.*' /etc/passwd;
+
+echo '====================================';
+echo "Listar usuarios de 4 caracteres: ";
+egrep '^....:.*:.*:.*:.*:.*:.*' /etc/passwd;
+
+echo '====================================';
+echo "Listar usuarios de 4 caracteres: ";
+egrep '^r...:.*:.*:.*:.*:.*:.*' /etc/passwd;
+
+
 
 
